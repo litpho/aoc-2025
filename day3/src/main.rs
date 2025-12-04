b4fe72e3fe01c0d10c13f37a5049bdab29ca6d09
Jasper de Vries <jasper.devries@the-future-group.com>
2025-12-04 17:56:24 +0100

    Day 3.2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -29,100 +29,39 @@ fn main() -> Result<()> {
 }
 
 fn part_one(input: &[Vec<u16>]) -> u64 {
-    input.iter().map(|row| calculate_line(row)).sum::<u64>()
-}
-
-fn calculate_line(input: &[u16]) -> u64 {
-    let length = input.len();
-    let mut digits: [usize; 2] = [0, 1];
-    for (i, val) in input.iter().enumerate().skip(2) {
-        if input[digits[1]] > input[digits[0]] && i < length {
-            digits[0] = digits[1];
-            digits[1] = i;
-        }
-        if val > &input[digits[1]] {
-            digits[1] = i;
-        }
-    }
-
-    (input[digits[0]] * 10 + input[digits[1]]) as u64
+    input.iter().map(|row| calculate_line(row, 2)).sum::<u64>()
 }
 
 fn part_two(input: &[Vec<u16>]) -> u64 {
-    input
-        .iter()
-        .skip(2)
-        // .take(1)
-        .map(|row| calculate_line_two(row))
-        .inspect(|val| println!("Val: {val}"))
-        .sum::<u64>()
+    input.iter().map(|row| calculate_line(row, 12)).sum::<u64>()
 }
 
-fn calculate_line_two(input: &[u16]) -> u64 {
-    let length = input.len();
-    let mut digits: [usize; 12] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
-    let mut moves = 0;
-    for (i, val) in input.iter().enumerate().skip(1) {
-        let mut found = false;
-        println!("i: {i}");
-        for j in 0..11 {
-            println!("j: {j}");
-            if input[digits[j + 1]] >= input[digits[j]] {
-                println!(
-                    "Moving {}({}) over {}({})",
-                    input[digits[j + 1]],
-                    digits[j + 1],
-                    input[digits[j]],
-                    digits[j]
-                );
-                digits[j] = digits[j + 1];
-                found = true;
-            } else if found {
-                println!("{} {}", digits[j], digits[j + 1]);
-                digits[j] = digits[j + 1];
+fn calculate_line(input: &[u16], length: usize) -> u64 {
+    let mut vec = input.to_vec();
+    'outer: while vec.len() > length {
+        for i in 1..vec.len() {
+            if vec[i - 1] < vec[i] {
+                vec.remove(i - 1);
+                continue 'outer;
             }
         }
-        debug(input, &mut digits);
-        println!("length: {length}, {moves} >= {}", length - 12);
-        if (val > &input[digits[11]] || found) && digits[11] < length - 1 {
-            println!("digits[11] {}", digits[11]);
-            digits[11] += 1;
-            moves += 1;
-            if moves >= length - 12 {
-                println!("break {digits:?}");
-                break;
-            }
-        }
-        println!("{digits:?}");
+
+        vec.remove(get_smallest_loc(&vec));
     }
 
-    println!("{digits:?}");
-
-    debug(input, &mut digits);
-
-    digits
-        .iter()
+    vec.iter()
         .enumerate()
-        .map(|(pos, i)| input[*i] as u64 * 10u64.pow((11 - pos) as u32))
+        .map(|(pos, i)| *i as u64 * 10u64.pow((length - 1 - pos) as u32))
         .sum::<u64>()
 }
 
-fn debug(input: &[u16], digits: &mut [usize; 12]) {
-    println!(
-        "{}{}{}{}{}{}{}{}{}{}{}{}",
-        input[digits[0]],
-        input[digits[1]],
-        input[digits[2]],
-        input[digits[3]],
-        input[digits[4]],
-        input[digits[5]],
-        input[digits[6]],
-        input[digits[7]],
-        input[digits[8]],
-        input[digits[9]],
-        input[digits[10]],
-        input[digits[11]]
-    );
+fn get_smallest_loc(input: &[u16]) -> usize {
+    input
+        .iter()
+        .enumerate()
+        .min_by(|(_, a), (_, b)| a.cmp(b))
+        .unwrap()
+        .0
 }
 
 fn parse(input: &'static str) -> Result<Vec<Vec<u16>>> {
@@ -150,11 +89,7 @@ mod tests {
 
     #[test]
     fn test_parse_one_testdata() -> Result<()> {
-        let vec = parse(TESTDATA)?;
-
-        println!("{:?}", vec);
-
-        let res = part_one(&vec);
+        let res = part_one(&parse(TESTDATA)?);
         assert_eq!(res, 357);
 
         Ok(())
@@ -168,19 +103,19 @@ mod tests {
         Ok(())
     }
 
-    // #[test]
-    // fn test_part_two_testdata() -> Result<()> {
-    //     let input = parse(TESTDATA)?;
-    //     assert_eq!(3121910778619, part_two(&input));
-    //
-    //     Ok(())
-    // }
+    #[test]
+    fn test_part_two_testdata() -> Result<()> {
+        let input = parse(TESTDATA)?;
+        assert_eq!(3121910778619, part_two(&input));
 
-    // #[test]
-    // fn test_part_two() -> Result<()> {
-    //     let input = parse(DATA)?;
-    //     assert_eq!(69553832684, part_two(&input));
-    //
-    //     Ok(())
-    // }
+        Ok(())
+    }
+
+    #[test]
+    fn test_part_two() -> Result<()> {
+        let input = parse(DATA)?;
+        assert_eq!(172740584266849, part_two(&input));
+
+        Ok(())
+    }
 }
